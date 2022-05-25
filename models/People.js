162ba3b50bc306d6996c7d743aaf141333c0e39c
Joe Park <sunghun.josephp@gmail.com>
2022-05-24 20:41:58 -0700

    Create People model

---
+++ models/People.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+// destructure the Schema and model from mongoose
+const { Schema, model } = mongoose
+
+const PeopleSchema = new Schema({
+  name: { type: String, required: true },
+  image: { type: String },
+  title: { type: String },
+});
+
+// give the People model a schema
+const People = model("People", PeopleSchema)
+
+// export the model
+module.exports = People
