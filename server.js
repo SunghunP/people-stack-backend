b1ffa5daafac207181d45325837f3b91bfd71657
Joe Park <sunghun.josephp@gmail.com>
2022-05-24 20:18:35 -0700

    Init commit

---
+++ server.js
@@ -0,0 +1,11 @@
+// --------------- //
+// Dependencies 
+// --------------- //
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5656;
+
+
