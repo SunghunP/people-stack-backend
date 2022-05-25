a927c9f26fe685a0249a608255aa17f21b83c889
Joe Park <sunghun.josephp@gmail.com>
2022-05-24 20:25:59 -0700

    Create Route for homepage and listener

--- server.js
+++ server.js
@@ -8,4 +8,10 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5656;
 
+// --------------- //
+// Routes 
+// --------------- //
+app.get("/", (req, res) => res.send("Hello World"));
 
+// Listener
+app.listen(PORT, () => console.log(`Listening on Port: ${PORT}...`))
