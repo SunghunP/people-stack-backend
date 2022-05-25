4ed2783155a477c22354f2a734fc2b74f5fd41ae
Joe Park <sunghun.josephp@gmail.com>
2022-05-24 21:01:17 -0700

    Create index route

--- server.js
+++ server.js
@@ -3,14 +3,21 @@
 // --------------- //
 require('dotenv').config();
 
+// pull PORT and DATABASE_URL from .env
+const { PORT = 5656, DATABASE_URL } = process.env;
+
 // import express and create app object
 const express = require('express');
 const app = express();
+
 const cors = require('cors');
+const morgan = require('morgan');
 const { default: mongoose } = require('mongoose');
 
-// pull PORT and DATABASE_URL from .env
-const { PORT = 5656, DATABASE_URL } = process.env;
+// --------------- //
+// Middleware
+// --------------- //
+app.use(morgan("dev")); // log every HTTP request
 
 // --------------- //
 // Database Connection 
@@ -26,7 +33,12 @@ mongoose.connection
 // --------------- //
 // Routes 
 // --------------- //
-app.get("/", (req, res) => res.send("Hello World"));
+const indexRouter = require('./routes/index'); // import index routes
+const peopleRouter = require('./routes/people'); // import people routes
+
+// let the app Use the routes
+app.use("/", indexRouter);
+app.use('/people', peopleRouter)
 
 // Listener
 app.listen(PORT, () => console.log(`Listening on Port: ${PORT}...`));
