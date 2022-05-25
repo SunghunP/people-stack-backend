a0eb13ab8e8d00ab3bb25aed8a571b8ab58bc27e
Joe Park <sunghun.josephp@gmail.com>
2022-05-24 20:33:05 -0700

    Create database connection

--- server.js
+++ server.js
@@ -3,10 +3,25 @@
 // --------------- //
 require('dotenv').config();
 
+// import express and create app object
 const express = require('express');
-const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 5656;
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+
+// pull PORT and DATABASE_URL from .env
+const { PORT = 5656, DATABASE_URL } = process.env;
+
+// --------------- //
+// Database Connection 
+// --------------- //
+mongoose.connect(DATABASE_URL);
+
+// Connection messages
+mongoose.connection
+  .on("open", () => console.log("Connection to MongoDB established!"))
+  .on("close", () => console.log("Lost connection to MongoDB!"))
+  .on("error", (error) => console.log(error));
 
 // --------------- //
 // Routes 
@@ -14,4 +29,4 @@ const PORT = process.env.PORT || 5656;
 app.get("/", (req, res) => res.send("Hello World"));
 
 // Listener
-app.listen(PORT, () => console.log(`Listening on Port: ${PORT}...`))
+app.listen(PORT, () => console.log(`Listening on Port: ${PORT}...`));
