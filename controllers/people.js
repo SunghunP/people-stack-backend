399ef8d61fba315a29450c8ae51e4addb2c44d52
Joe Park <sunghun.josephp@gmail.com>
2022-05-24 22:08:55 -0700

    Establish Update Route

--- controllers/people.js
+++ controllers/people.js
@@ -3,8 +3,10 @@ const People = require("../models/people");
 module.exports = {
   index,
   create,
+  update,
 }
 
+// People Index Route
 async function index(req, res) {
   try {
     // fetch all people from our DB
@@ -15,9 +17,23 @@ async function index(req, res) {
   }
 }
 
+// People Update Route
+async function update(req, res) {
+  try {
+    // send all people
+    res.json(
+      await People.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    );
+  } catch (err) {
+    // send error
+    res.status(400).json(err);
+  }
+} 
+
+// People Create Route
 async function create(req, res) {
   try {
-    // send all people to the DB
+    // send all people 
     res.json(await People.create(req.body));
   } catch (err) {
     // send the error 
--- routes/people.js
+++ routes/people.js
@@ -4,6 +4,7 @@ const peopleCtrl = require('../controllers/people');
 
 // INDUCES Routes
 router.get('/', peopleCtrl.index); // Index Route
+router.put('/:id', peopleCtrl.update); // Update Route
 router.post('/', peopleCtrl.create); // Create Route
 
 module.exports = router;
